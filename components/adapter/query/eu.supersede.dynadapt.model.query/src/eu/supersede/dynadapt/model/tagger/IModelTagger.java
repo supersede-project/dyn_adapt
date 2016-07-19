9bd95b40ad55694bf72e05df93b82ebb6f252a72
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2016-07-19 15:51:01 +0200

    folder renamed

--- components/adapter/query/eu.supersede.dynadapt.model.query/src/eu/supersede/dynadapt/model/tagger/IModelTagger.java
+++
@@ -1,9 +0,0 @@
-package eu.supersede.dynadapt.model.tagger;
-
-import org.eclipse.uml2.uml.NamedElement;
-import org.eclipse.uml2.uml.Profile;
-
-public interface IModelTagger {
-	public void tagModel(NamedElement element, Profile profile, String stereotypeName) throws Exception;
-	public void tagModel(NamedElement element, String profileURI, String stereotypeName) throws Exception;
-}
