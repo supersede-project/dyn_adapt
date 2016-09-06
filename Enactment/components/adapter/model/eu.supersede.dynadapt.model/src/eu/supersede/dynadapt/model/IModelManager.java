4a30a7014ac92f5d27d02e88925caa7d87bdf8a0
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2016-09-06 11:37:55 +0200

    Improve UML resource registration

---
+++ Enactment/components/adapter/model/eu.supersede.dynadapt.model/src/eu/supersede/dynadapt/model/IModelManager.java
@@ -0,0 +1,69 @@
+package eu.supersede.dynadapt.model;
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.uml2.uml.Profile;
+
+public interface IModelManager {
+
+	/**
+	 * Loads the UML base model resource from the passed file and returns this Resource object.
+	 * 
+	 * @param file
+	 *            IFile object to be loaded as the resource
+	 * @return Resource object loaded from the passed file
+	 */
+	Resource loadResource(String targetModelPath);
+
+	/**
+	 * Loads the profile of the given name and returns this Profile object.
+	 * 
+	 * @param profileFileName
+	 *            Name of the file to be loaded as a profile
+	 * @return Profie loaded from the file
+	 */
+	Profile loadProfile(String profilePath);
+
+	/**
+	 * Returns the associated UML base target model
+	 * @return the associated UML base target model
+	 */
+	Resource getTargetModel();
+
+	/**
+	 * @throws IOException 
+	 * Save a model resource included in current resource set, into the outputModelURI recipient, adding suffixe to that URI
+	 * @param modelResource
+	 * @param outputModelURI
+	 * @param suffixe
+	 * @throws  
+	 */
+	URI saveModel(Resource modelResource, URI outputModelURI, String suffixe) throws IOException;
+
+	/**
+	 * Saves a copy of associated target model adding given suffixe to the original target model name
+	 * @param suffixe suffixe added to the original target model name 
+	 * @return locator for saved model
+	 * @throws IOException
+	 */
+	URI saveTargetModel(String suffixe) throws IOException;
+	
+	/**
+	 * Saves a associated target model replacing original file
+	 * @return locator for saved model
+	 * @throws IOException
+	 */
+	URI saveTargetModel() throws IOException;
+	
+	/**
+	 * Regiser given model into the ModelManager resource set
+	 * @param ePackage model to register
+	 * @see org.eclipse.uml2.uml.resource.UMLResource
+	 */
+	void registerPackage (EPackage ePackage);
+
+}
