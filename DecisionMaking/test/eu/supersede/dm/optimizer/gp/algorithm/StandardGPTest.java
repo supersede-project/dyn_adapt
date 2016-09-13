3d54250dd0a75ffd2f07dd034aa6971a62dff8eb
Fitsum Kifetew <fitsumm@gmail.com>
2016-09-13 12:03:36 +0200

    added tests

---
+++ DecisionMaking/test/eu/supersede/dm/optimizer/gp/algorithm/StandardGPTest.java
@@ -0,0 +1,30 @@
+package eu.supersede.dm.optimizer.gp.algorithm;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import eu.supersede.dm.optimizer.gp.Parameters;
+import eu.supersede.dm.optimizer.gp.chromosome.Chromosome;
+
+public class StandardGPTest {
+
+	@Test
+	public void testGenerateSolution() {
+		String grammarFile = Parameters.GRAMMAR_FILE;
+		int depth = 6;
+		double probRecursive = 0.1;
+		Parameters.SEARCH_BUDGET = 100;
+		Parameters.POPULATION_SIZE = 10;
+		StandardGP algorithm = new StandardGP(grammarFile, depth, probRecursive);
+		List<Chromosome> solutions = algorithm.generateSolution();
+		System.out.printf("Finished after %d generations and %d fitness evaluations.", algorithm.getGenerations(),algorithm.getFitnessEvaluations());
+		System.out.println();
+		for (Chromosome solution : solutions){
+			System.out.println("[" + solution.getFitness() + "] " + solution.toString());
+		}
+	}
+
+}
