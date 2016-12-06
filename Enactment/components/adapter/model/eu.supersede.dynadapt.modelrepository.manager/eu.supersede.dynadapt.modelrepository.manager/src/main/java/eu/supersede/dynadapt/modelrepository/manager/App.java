e057919dc908b87d976c57a60488325a24455a5f
jmotger <joaquim.motger@est.fib.upc.edu>
2016-12-06 18:33:51 +0100

    added model repository manager project

---
+++ Enactment/components/adapter/model/eu.supersede.dynadapt.modelrepository.manager/eu.supersede.dynadapt.modelrepository.manager/src/main/java/eu/supersede/dynadapt/modelrepository/manager/App.java
@@ -0,0 +1,13 @@
+package eu.supersede.dynadapt.modelrepository.manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
