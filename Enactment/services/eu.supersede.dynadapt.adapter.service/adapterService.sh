e4b5e987657993b30a8913b5c26c86e2ac911377
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2016-12-21 10:15:13 +0100

    scripts to launch Adapter

---
+++ Enactment/services/eu.supersede.dynadapt.adapter.service/adapterService.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+SPRING_APPLICATION_JSON='{"server.port":8090}' /home/yosu/Projects/Supersede/Git/dyn_adapt/Enactment/services/eu.supersede.dynadapt.adapter.service/target/eu.supersede.dynadapt.adapter.service-1.0.0-SNAPSHOT.jar
