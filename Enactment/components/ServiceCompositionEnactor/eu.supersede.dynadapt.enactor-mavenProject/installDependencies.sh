cd3631ab347dca4a093be91833cb675aa19da0b0
Stevanetic <srdjan.stevanetic@siemens.com>
2017-02-16 17:35:44 +0100

    a

--- Enactment/components/ServiceCompositionEnactor/eu.supersede.dynadapt.enactor-mavenProject/installDependencies.sh
+++ Enactment/components/ServiceCompositionEnactor/eu.supersede.dynadapt.enactor-mavenProject/installDependencies.sh
@@ -2,7 +2,6 @@ mvn install:install-file -DgroupId=org.eclipse.emf.ecore.xmi -DartifactId=org-ec
 
 mvn install:install-file -DgroupId=org.eclipse.uml2 -DartifactId=org-eclipse-uml2-uml -Dversion=5.2.2.v20161114-0827 -Dfile=../../required-JARS/org.eclipse.uml2.uml_5.2.2.v20161114-0827.jar -Dpackaging=jar
 
-<<<<<<< HEAD
 mvn install:install-file -DgroupId=ptsupport -DartifactId=ptsupport -Dversion=11.0 -Dfile=../../required-JARS/ptsupport.jar -Dpackaging=jar
 
 mvn install:install-file -DgroupId=org.eclipse.emf.gwt.common -DartifactId=gwt-common -Dversion=2.9.0 -Dfile=../../required-JARS/org.eclipse.emf.gwt.common-2.9.0.jar -Dpackaging=jar
@@ -13,19 +12,6 @@ mvn install:install-file -DgroupId=ptolemy.domains -DartifactId=ptolemy-domains 
 
 mvn install:install-file -DgroupId=ptolemy.actor.lib.jjs -DartifactId=ptolemy-actor-lib-jjs -Dversion=11.0 -Dfile=../../required-JARS/jjs.jar -Dpackaging=jar
 
-=======
-
-mvn install:install-file -DgroupId=ptsupport -DartifactId=ptsupport -Dversion=11.0 -Dfile=../../required-JARS/ptsupport.jar -Dpackaging=jar
-
-mvn install:install-file -DgroupId=org.eclipse.emf.gwt.common -DartifactId=gwt-common -Dversion=2.9.0 -Dfile=../../required-JARS/org.eclipse.emf.gwt.common-2.9.0.jar -Dpackaging=jar
-
-mvn install:install-file -DgroupId=org.eclipse.emf.gwt.ecore -DartifactId=gwt-ecore -Dversion=2.9.0 -Dfile=../../required-JARS/org.eclipse.emf.gwt.ecore-2.9.0.jar -Dpackaging=jar
-
-mvn install:install-file -DgroupId=ptolemy.domains -DartifactId=ptolemy-domains -Dversion=11.0 -Dfile=../../required-JARS/domains.jar -Dpackaging=jar
-
-mvn install:install-file -DgroupId=ptolemy.actor.lib.jjs -DartifactId=ptolemy-actor-lib-jjs -Dversion=11.0 -Dfile=../../required-JARS/jjs.jar -Dpackaging=jar
-
->>>>>>> 28e8a53747c0e3e22893c7f288ce9574c93f43db
 mvn install:install-file -DgroupId=ptolemy.supersede -DartifactId=ptolemy-supersede -Dversion=11.0 -Dfile=../../required-JARS/ptolemy-supersede.jar -Dpackaging=jar
 
 mvn install:install-file -DgroupId=com.google.gwt.servlet -DartifactId=google-gwt-servlet -Dversion=2.0.4.v20100709-0658 -Dfile=../../required-JARS/com.google.gwt.servlet_2.0.4.v20100709-0658.jar -Dpackaging=jar
@@ -43,15 +29,8 @@ mvn install:install-file -DgroupId=org.eclipse.uml2.common -DartifactId=org-ecli
 mvn install:install-file -DgroupId=org.eclipse.emf.mapping.ecore2xml -DartifactId=org-eclipse-emf-mapping-ecore2xml -Dversion=2.9.0.v20160526-0356 -Dfile=../../required-JARS/org.eclipse.emf.mapping.ecore2xml_2.9.0.v20160526-0356.jar -Dpackaging=jar
 
 mvn install:install-file -DgroupId=ptolemy.de -DartifactId=ptolemy-de -Dversion=11.0 -Dfile=../../required-JARS/de.jar -Dpackaging=jar
-<<<<<<< HEAD
 
 mvn install:install-file -DgroupId=ptolemy.json -DartifactId=ptolemy-json -Dversion=11.0 -Dfile=../../required-JARS/conversions-json.jar -Dpackaging=jar
-
-mvn install:install-file -DgroupId=ptolemy.org.json -DartifactId=ptolemy-org-json -Dversion=11.0 -Dfile=../../required-JARS/org-json.jar -Dpackaging=jar
-=======
-
-mvn install:install-file -DgroupId=ptolemy.json -DartifactId=ptolemy-json -Dversion=11.0 -Dfile=../../required-JARS/conversions-json.jar -Dpackaging=jar
->>>>>>> 28e8a53747c0e3e22893c7f288ce9574c93f43db
 
 mvn install:install-file -DgroupId=ptolemy.org.json -DartifactId=ptolemy-org-json -Dversion=11.0 -Dfile=../../required-JARS/org-json.jar -Dpackaging=jar
 
