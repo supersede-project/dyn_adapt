5f8bdde6172f729179bf46ab379f58a34ffd976b
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-02-16 17:10:03 +0100

    added script to install required dependencies for Service Composition Enactor

---
+++ Enactment/components/ServiceCompositionEnactor/eu.supersede.dynadapt.enactor-mavenProject/installDependencies.sh
@@ -0,0 +1,37 @@
+mvn install:install-file -DgroupId=org.eclipse.emf.ecore.xmi -DartifactId=org-eclipse-emf-ecore-xmi -Dversion=2.12.0.v20160420-0247 -Dfile=../../required-JARS/org.eclipse.emf.ecore.xmi_2.12.0.v20160420-0247.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.uml2 -DartifactId=org-eclipse-uml2-uml -Dversion=5.2.2.v20161114-0827 -Dfile=../../required-JARS/org.eclipse.uml2.uml_5.2.2.v20161114-0827.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptsupport -DartifactId=ptsupport -Dversion=11.0 -Dfile=../../required-JARS/ptsupport.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.emf.gwt.common -DartifactId=gwt-common -Dversion=2.9.0 -Dfile=../../required-JARS/org.eclipse.emf.gwt.common-2.9.0.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.emf.gwt.ecore -DartifactId=gwt-ecore -Dversion=2.9.0 -Dfile=../../required-JARS/org.eclipse.emf.gwt.ecore-2.9.0.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptolemy.domains -DartifactId=ptolemy-domains -Dversion=11.0 -Dfile=../../required-JARS/domains.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptolemy.actor.lib.jjs -DartifactId=ptolemy-actor-lib-jjs -Dversion=11.0 -Dfile=../../required-JARS/jjs.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptolemy.supersede -DartifactId=ptolemy-supersede -Dversion=11.0 -Dfile=../../required-JARS/ptolemy-supersede.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=com.google.gwt.servlet -DartifactId=google-gwt-servlet -Dversion=2.0.4.v20100709-0658 -Dfile=../../required-JARS/com.google.gwt.servlet_2.0.4.v20100709-0658.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.apache.servicemix.bundles -DartifactId=org-apache-servicemix-bundles-gwt-user -Dversion=2.4.0_1 -Dfile=../../required-JARS/org.apache.servicemix.bundles.gwt-user-2.4.0_1.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.uml2.types -DartifactId=org-eclipse-uml2-types -Dversion=2.0.0.v20161114-0827 -Dfile=../../required-JARS/org.eclipse.uml2.types_2.0.0.v20161114-0827.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.emf.common -DartifactId=org-eclipse-emf-common -Dversion=2.12.0.v20160420-0247 -Dfile=../../required-JARS/org.eclipse.emf.common_2.12.0.v20160420-0247.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.emf.ecore -DartifactId=org-eclipse-emf-ecore -Dversion=2.12.0.v20160420-0247 -Dfile=../../required-JARS/org.eclipse.emf.ecore_2.12.0.v20160420-0247.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.uml2.common -DartifactId=org-eclipse-uml2-common -Dversion=2.1.0.v20161114-0827 -Dfile=../../required-JARS/org.eclipse.uml2.common_2.1.0.v20161114-0827.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=org.eclipse.emf.mapping.ecore2xml -DartifactId=org-eclipse-emf-mapping-ecore2xml -Dversion=2.9.0.v20160526-0356 -Dfile=../../required-JARS/org.eclipse.emf.mapping.ecore2xml_2.9.0.v20160526-0356.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptolemy.de -DartifactId=ptolemy-de -Dversion=11.0 -Dfile=../../required-JARS/de.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptolemy.json -DartifactId=ptolemy-json -Dversion=11.0 -Dfile=../../required-JARS/json.jar -Dpackaging=jar
+
+mvn install:install-file -DgroupId=ptolemy.org.json -DartifactId=ptolemy-org-json -Dversion=11.0 -Dfile=../../required-JARS/org-json.jar -Dpackaging=jar
+
+
