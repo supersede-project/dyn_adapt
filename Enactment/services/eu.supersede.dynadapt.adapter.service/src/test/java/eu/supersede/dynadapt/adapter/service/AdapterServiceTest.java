30e22fa110687be4ca8a94a320456e8f9105f316
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-04-03 22:55:27 +0200

    Fixed misconfiguration in Adapter service test and general building script

--- Enactment/services/eu.supersede.dynadapt.adapter.service/src/test/java/eu/supersede/dynadapt/adapter/service/AdapterServiceTest.java
+++ Enactment/services/eu.supersede.dynadapt.adapter.service/src/test/java/eu/supersede/dynadapt/adapter/service/AdapterServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import eu.supersede.dynadapt.adapter.exception.EnactmentException;
-import eu.supersede.dynadapt.adapter.system.SupersedeSystem;
 import eu.supersede.integration.api.adaptation.types.ModelSystem;
 
 public class AdapterServiceTest {
@@ -18,34 +17,14 @@ public class AdapterServiceTest {
 	}
 	
 	@Test
-	public void testAtosCMSUCAdaptation() {
-		try {
-			//FIXME featureConfigurationId is ignored. Use correct one
-			//once Model Repository is available as service.
-			String[] adaptationDecisionActionIds = new String[]{"cms_optimal_configuration", "cms_standard_configuration"};
-			String featureConfigurationId = null;
-			service.enactAdaptationDecisionActions(
-					ModelSystem.Atos.toString(), Arrays.asList(adaptationDecisionActionIds), featureConfigurationId);
-//			service.enactAdaptationDecisionAction(
-//					ModelSystem.Atos.toString(), adaptationDecisionActionIds[0], featureConfigurationId);
-		} catch (EnactmentException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
 	public void testAtosHSKUCAdaptation() {
 		try {
 			//FIXME featureConfigurationId is ignored. Use correct one
 			//once Model Repository is available as service.
 			String[] adaptationDecisionActionIds = new String[]{"highloadconfigurationinvm2_a", "lowloadconfigurationinvm2_a"};
-			String featureConfigurationId = null;
+			String featureConfigurationId = "SmartPlatformFC_HSK_HighLoad";
 			service.enactAdaptationDecisionActions(
 					ModelSystem.Atos_HSK.toString(), Arrays.asList(adaptationDecisionActionIds), featureConfigurationId);
-//			service.enactAdaptationDecisionAction(
-//					ModelSystem.Atos.toString(), adaptationDecisionActionIds[0], featureConfigurationId);
 		} catch (EnactmentException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
