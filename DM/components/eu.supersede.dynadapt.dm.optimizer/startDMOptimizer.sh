d11389b1189e8ece722e81ec5e060ccfdf3da4f1
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-04-28 15:28:42 +0200

    Patch for DM Optimizer standalone execution

--- DM/components/eu.supersede.dynadapt.dm.optimizer/startDMOptimizer.sh
+++ DM/components/eu.supersede.dynadapt.dm.optimizer/startDMOptimizer.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -Dserver.port=8091 -jar target/eu.supersede.dynadapt.dm.optimizer-0.0.1-SNAPSHOT.jar --logging.file=./log/dm-optimizer.log
+#FIXME: using java with jar raises an exception when loading Acceleo MTFL files, because not registered editor. Investigate
+#classpaht execution between java and mvn
+#java -Dserver.port=8091 -jar target/eu.supersede.dynadapt.dm.optimizer-0.0.1-SNAPSHOT.jar --logging.file=./log/dm-optimizer.log
+mvn spring-boot:run -Drun.arguments="--server.port=8091,--logging.file=./log/dm-optimizer.log"
