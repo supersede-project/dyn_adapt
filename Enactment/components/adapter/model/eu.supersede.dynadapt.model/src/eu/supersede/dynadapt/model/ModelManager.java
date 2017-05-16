6206f5fcd65d82d7fed139b96c5020bed72d9b44
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-05-16 12:55:57 +0200

    Fix: patch to manage correctly Windows OS paths

--- Enactment/components/adapter/model/eu.supersede.dynadapt.model/src/eu/supersede/dynadapt/model/ModelManager.java
+++ Enactment/components/adapter/model/eu.supersede.dynadapt.model/src/eu/supersede/dynadapt/model/ModelManager.java
@@ -380,10 +380,17 @@ public class ModelManager implements IModelManager {
 	 */
 	//TODO: Simplify this method using Java8 io library
 	private File createOutputFile(URI outputModelURI, String suffixe) throws Exception {
+		String fileSeparator = System.getProperty("file.separator");
 		String inputFileName = outputModelURI.lastSegment();
+		if (inputFileName == null){
+			inputFileName = outputModelURI.toString().substring (outputModelURI.toString().lastIndexOf(fileSeparator)+1);
+		}
 		String inputFilePath = outputModelURI.path();
+		if (inputFilePath == null){
+			inputFilePath = outputModelURI.toString().substring(0, outputModelURI.toString().lastIndexOf(fileSeparator));
+		}
 		// Find output directory
-		String outputDirectory = inputFilePath.substring(0, (inputFilePath.lastIndexOf('/') + 1));
+		String outputDirectory = inputFilePath.substring(0, (inputFilePath.lastIndexOf(fileSeparator) + 1));
 		//Create outputDirectory if it doesn't exist
 		Path outDir = Paths.get(outputDirectory);
 		if (Files.notExists(outDir)) {
