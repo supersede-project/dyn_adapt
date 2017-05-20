d5c56e9be9455cf156d21d2821cd6fc07a5891c6
jmotger <joaquim.motger@est.fib.upc.edu>
2017-05-20 17:57:24 +0200

    init adaptation dashboard project

---
+++ Enactment/components/adapter/model/adaptation-dashboard/src/main/java/eu/supersede/dynadapt/adaptation/dashboard/AdaptationDashboardApplication.java
@@ -0,0 +1,12 @@
+package eu.supersede.dynadapt.adaptation.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptationDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaptationDashboardApplication.class, args);
+	}
+}
