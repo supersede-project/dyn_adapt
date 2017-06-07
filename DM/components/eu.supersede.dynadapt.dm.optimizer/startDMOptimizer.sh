3c0cc4bdef190a228524b109b76af48898b2144b
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-06-07 13:15:00 +0200

    DM Optimizer listening for alerts send to the production environment

--- DM/components/eu.supersede.dynadapt.dm.optimizer/startDMOptimizer.sh
+++ DM/components/eu.supersede.dynadapt.dm.optimizer/startDMOptimizer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #FIXME: using java with jar raises an exception when loading Acceleo MTFL files, because not registered editor. Investigate
 #classpaht execution between java and mvn
-#java -Dserver.port=8091 -jar target/eu.supersede.dynadapt.dm.optimizer-0.0.1-SNAPSHOT.jar --logging.file=./log/dm-optimizer.log
-mvn spring-boot:run -Drun.arguments="--server.port=8091,--logging.file=./log/dm-optimizer.log"
+#java -Dserver.port=8091 -Dsupersede.if.properties=if.production.properties -jar target/eu.supersede.dynadapt.dm.optimizer-0.0.1-SNAPSHOT.jar --logging.file=./log/dm-optimizer.log
+mvn spring-boot:run -Drun.arguments="--server.port=8091,--logging.file=./log/dm-optimizer.log" -Dsupersede.if.properties=if.production.properties
