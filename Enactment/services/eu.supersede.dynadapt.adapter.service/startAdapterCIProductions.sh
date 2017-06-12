66e739abe742d91e298b2585ffb0ee473f2b2e62
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-06-12 12:59:24 +0200

    Scripts for launching Adapter in both production or developmente environments

--- Enactment/services/eu.supersede.dynadapt.adapter.service/startAdapterCIProductions.sh
+++ Enactment/services/eu.supersede.dynadapt.adapter.service/startAdapterCIProductions.sh
@@ -2,4 +2,4 @@
 #FIXME When launched with the Java Jar file, the ServiceCompositionEnactor does not work correctly raising an ArrayOutOfBounds.
 #Investigate possible dependency issue
 #BUILD_ID=dontKillMe nohup java -Dserver.port=8090 -jar target/eu.supersede.dynadapt.adapter.service-1.0.0-SNAPSHOT.jar --logging.file=./log/adapter.log &
-BUILD_ID=dontKillMe nohup mvn spring-boot:run -Drun.arguments="--server.port=8090,--logging.file=./log/adapter.log" &
+BUILD_ID=dontKillMe nohup mvn spring-boot:run -Drun.arguments="--server.port=8090,--logging.file=./log/adapter.log" -Dsupersede.if.properties=if.production.properties &
