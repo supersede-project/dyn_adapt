9d5198dae11d85d67dcc05c8a39e686165154256
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-05-30 12:25:03 +0200

    Adapter launchers for production

--- Enactment/services/eu.supersede.dynadapt.adapter.service/startAdapterProduction.sh
+++ Enactment/services/eu.supersede.dynadapt.adapter.service/startAdapterProduction.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 #FIXME When launched with the Java Jar file, the ServiceCompositionEnactor does not work correctly raising an ArrayOutOfBounds.
 #Investigate possible dependency issue
-#java -Dserver.port=8090 -jar target/eu.supersede.dynadapt.adapter.service-1.0.0-SNAPSHOT.jar --logging.file=./log/adapter.log
-mvn spring-boot:run -Drun.arguments="--server.port=8090,--logging.file=./log/adapter.log" -Dsupersede.if.properties=if.production.properties
+
+file="./supersede_account"
+if [ -f "$file" ]
+then
+user=`sed '/^\#/d' $file | grep 'is.admin.user'  | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+pass=`sed '/^\#/d' $file | grep 'is.admin.passwd'  | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+else
+  echo "$file not found."
+fi
+
+nohup mvn spring-boot:run -Drun.arguments="--server.port=8092,--logging.file=./log/adapter.log" -Dsupersede.if.properties=if.production.properties -Dfg.admin.user=$user -Dfg.admin.passwd=$pass &
 
